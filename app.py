5be8a2576af80b960cc4ee2665dd57e735ca534d
Fernando Celmer <fernando.celmer@cakeerp.com>
2021-03-14 02:01:41 -0300

    app

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def nao_entre_em_panico():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='https://indexoffy.herokuapp.com/', port=port)
