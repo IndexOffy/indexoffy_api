c1d14ff85fff20a18e2d26527274acc4594d0c69
FernandoCelmer <email@fernandocelmer.com>
2021-03-20 20:34:34 -0300

    migrations

---
+++ migrations/versions/fecd3b51e7e4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fecd3b51e7e4
+Revises: 24152e2f4bfd
+Create Date: 2021-03-20 18:31:47.624921
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fecd3b51e7e4'
+down_revision = '24152e2f4bfd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
