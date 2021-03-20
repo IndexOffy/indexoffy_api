92e3b9da317c4dc5c305efa26c8c9ab3f8abc9a1
FernandoCelmer <email@fernandocelmer.com>
2021-03-19 23:18:12 -0300

    init

--- indexoffy/app/__init__.py
+++ indexoffy/app/__init__.py
@@ -34,14 +34,18 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.mod_auth.controllers import mod_auth
-from app.mod_home.controllers import mod_home
+from app.modules.mod_home.controllers import mod_home
+from app.modules.mod_user.controllers import mod_user
+
+from app.models.api_token import ApiToken
+from app.models.base_customer import BaseCustomer
 
 # Register blueprint(s)
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 app.register_blueprint(mod_home)
-app.register_blueprint(mod_auth)
+app.register_blueprint(mod_user)
 
 # Build the database:
 # This will create the database file using SQLAlchemy
-db.create_all()
+db.init_app(app)
+migrate.init_app(app, db)
