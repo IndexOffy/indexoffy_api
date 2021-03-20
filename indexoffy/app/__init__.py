09b69e8a5bbba652adb7fb85021302629e6c58ba
FernandoCelmer <email@fernandocelmer.com>
2021-03-20 16:39:12 -0300

    app

--- indexoffy/app/__init__.py
+++ indexoffy/app/__init__.py
@@ -5,6 +5,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_alembic import Alembic
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -26,7 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Define the database object which is imported
 # by modules and controllers
 db = SQLAlchemy(app)
-migrate = Migrate()
+migrate = Migrate(app, db)
 
 # Sample HTTP error handling
 @app.errorhandler(404)
@@ -37,15 +38,17 @@ def not_found(error):
 from app.modules.mod_home.controllers import mod_home
 from app.modules.mod_user.controllers import mod_user
 
-from app.models.api_token import ApiToken
-from app.models.base_customer import BaseCustomer
-
 # Register blueprint(s)
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 app.register_blueprint(mod_home)
 app.register_blueprint(mod_user)
 
+from app.models.api_token import ApiToken
+from app.models.api_user import ApiUser
+from app.models.base_customer import BaseCustomer
+
 # Build the database:
 # This will create the database file using SQLAlchemy
 db.init_app(app)
 migrate.init_app(app, db)
+db.create_all()
