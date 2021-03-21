577bd9226ed230141559d02a74d78e8dfb4a5365
FernandoCelmer <email@fernandocelmer.com>
2021-03-21 20:37:07 -0300

    api user

--- indexoffy/app/modules/api_user/controllers.py
+++ indexoffy/app/modules/api_user/controllers.py
@@ -3,6 +3,8 @@ from flask import Blueprint, jsonify, redirect, url_for, request
 
 from app import db
 from app.modules.api.base import BaseApi
+from app.modules.api.utils.responses import BaseResponse
+
 from app.models.user import User, user_schema, users_schema
 
 mod_user = Blueprint('users', __name__, url_prefix='/users')
@@ -11,42 +13,42 @@ mod_user = Blueprint('users', __name__, url_prefix='/users')
 
 class ViewUser(BaseApi):
 
+
     @mod_user.route('/', methods=['GET'])
     @BaseApi.validate_token
-    def get_user_code(data):
-        print(data)
+    def get_user(data):
         try:
             user = User.query.filter(User.code == data['params'].get('code')).first()
             if user:
                 result = user_schema.dump(user)
-                return jsonify({"message": "successfully fetched", "data": result}), 200
+                BaseResponse().successfully_fetched(result, data)
         except:
-            return jsonify({"message": "error", "data":{}}), 500
+            return BaseResponse().server_error()
 
-        return jsonify({"message": "user don't exist", "data":{}}), 404
+        return BaseResponse().user_dont_exist()
 
     @mod_user.route('/all', methods=['GET'])
     @BaseApi.validate_token_admin
-    def get_all(data):
+    def get_all_admin(data):
         try:
             users = User.query.all()
             if users:
                 result = users_schema.dump(users)
-            return jsonify({"message": "successfully fetched", "data": result}), 200
+            BaseResponse().successfully_fetched(result)
         except:
-            return jsonify({"message": "error", "data":{}}), 500
+            return BaseResponse().server_error()
         
-        return jsonify({"message": "user don't exist", "data":{}}), 404
+        return BaseResponse().user_dont_exist()
 
     @mod_user.route('/<id>', methods=['GET'])
     @BaseApi.validate_token_admin
-    def get_user(data, *args, **kwargs):
+    def get_user_admin(data, *args, **kwargs):
         try:
             user = User.query.filter(User.id == data['params']['id']).first()
             if user:
                 result = user_schema.dump(user)
-                return jsonify({"message": "successfully fetched", "data": result}), 200
+                BaseResponse().successfully_fetched(result)
         except:
-            return jsonify({"message": "error", "data":{}}), 500
+            return BaseResponse().server_error()
 
-        return jsonify({"message": "user don't exist", "data":{}}), 404
+        return BaseResponse().user_dont_exist()
