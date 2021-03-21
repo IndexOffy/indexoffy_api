4db943183f232d35fa6a8d91552cd494cb55c5c1
FernandoCelmer <email@fernandocelmer.com>
2021-03-20 22:52:47 -0300

    debug False

--- indexoffy/run.py
+++ indexoffy/run.py
@@ -3,7 +3,7 @@ from app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    debug = os.environ.get("DEBUG", True)
+    debug = os.environ.get("DEBUG", False)
     host = os.environ.get("HOST", "localhost")
     
     app.run(host=host, port=port, debug=debug)
