46c73f6e2cbf2a4bc3f7456a40e00d6a12abd5a5
FernandoCelmer <email@fernandocelmer.com>
2021-03-21 23:03:37 -0300

    Resposta padrões da API

--- indexoffy/app/modules/api/utils/responses.py
+++ indexoffy/app/modules/api/utils/responses.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+import inspect
+
+from app import db
 from flask import jsonify
 from app.models.base_log import BaseLog
 
@@ -7,24 +10,108 @@ from app.models.base_log import BaseLog
 class BaseResponse(object):
     """ Base View to create helpers common to all Webservices.
     """
-    def __init__(self):
+    def __init__(self, token=None, data=None, model_class="model_class", operation="Error"):
         """Constructor
         """
-        pass
+        if not data:
+            self.base_log_data = {
+                "base_customer": 0,
+                "user_name": "No name",
+                "user_email": "No email",
+                "operation": operation,
+                "model_class": model_class,
+                "model_id": 0,
+                "params": str({}),
+                "args":  str({}),
+            }
+        else:
+            self.base_log_data = {
+                "base_customer": data['user'].base_customer,
+                "user_name": data['user'].name,
+                "user_email": data['user'].email,
+                "operation": operation,
+                "model_class": model_class,
+                "model_id": 0,
+                "params": str(data['params']),
+                "args":  str(data['args']),
+            }
 
-    def successfully_fetched(self, result, data):
+    def successfully_fetched(self, result=None):
         """ 
         """
-        return jsonify({"message": "successfully fetched", "data": result}), 200
+        status = 200
+        try:
+            self.base_log_data["message"] = "successfully_fetched"
+            self.base_log_data["status"] = status
+            self.base_log_data["result"] = str(result)
+
+            base_log = BaseLog(**self.base_log_data)
+            db.session.add(base_log)
+            db.session.commit()
+        except:
+            pass
+        return jsonify({"message": "successfully fetched", "data": result}), status
     
-    def server_error(self):
-        return jsonify({"message": "error", "data":{}}), 500
+    def server_error(self, result=None):
+        """ 
+        """
+        status = 500
+        try:
+            self.base_log_data["message"] = "server_error"
+            self.base_log_data["status"] = status
+            self.base_log_data["result"] = str(result)
 
-    def user_dont_exist(self):
-        return jsonify({"message": "user don't exist", "data":{}}), 404
+            base_log = BaseLog(**self.base_log_data)
+            db.session.add(base_log)
+            db.session.commit()
+        except:
+            pass
+        return jsonify({"message": "error", "data":{}}), status
 
-    def token_is_missing(self):
-        return jsonify({'message': 'token is missing', 'data': {}}), 401
+    def user_dont_exist(self, result=None):
+        """ 
+        """
+        status = 404
+        try:
+            self.base_log_data["message"] = "user_dont_exist"
+            self.base_log_data["status"] = status
+            self.base_log_data["result"] = str(result)
 
-    def token_is_invalid(self):
-        return jsonify({'message': 'token is invalid', 'data': {}}), 401
+            base_log = BaseLog(**self.base_log_data)
+            db.session.add(base_log)
+            db.session.commit()
+        except:
+            pass
+        return jsonify({"message": "user don't exist", "data":{}}), status
+
+    def token_is_missing(self, result=None):
+        """ 
+        """
+        status = 401
+        try:
+            self.base_log_data["message"] = "token_is_missing"
+            self.base_log_data["status"] = status
+            self.base_log_data["result"] = str(result)
+
+            base_log = BaseLog(**self.base_log_data)
+            db.session.add(base_log)
+            db.session.commit()
+        except:
+            pass
+        return jsonify({'message': 'token is missing', 'data': {}}), status
+
+    def token_is_invalid(self, result=None):
+        """ 
+        """
+        status = 401
+        try:
+            self.base_log_data["message"] = "token_is_invalid"
+            self.base_log_data["status"] = status
+            self.base_log_data["result"] = str(result)
+
+            base_log = BaseLog(**self.base_log_data)
+            db.session.add(base_log)
+            db.session.commit()
+        except:
+            pass
+        return jsonify({'message': 'token is invalid', 'data': {}}), status
