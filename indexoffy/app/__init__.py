5d38b2ed04bf449696fd53bf8773631d693c046c
FernandoCelmer <email@fernandocelmer.com>
2021-03-23 02:35:33 -0300

    Alteração imports models e routes

--- indexoffy/app/__init__.py
+++ indexoffy/app/__init__.py
@@ -36,8 +36,8 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.modules.public.api_doc.routers import mod_doc as doc
-from app.modules.public.api_user.routers import mod_user as user
+from app.modules.public.api_doc.routes import mod_doc as doc
+from app.modules.public.api_user.routes import mod_user as user
 
 # Register blueprint(s)
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
