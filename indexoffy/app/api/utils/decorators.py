951d19be49083abacc792a068f5a4df3d46fde95
FernandoCelmer <email@fernandocelmer.com>
2021-03-23 02:36:35 -0300

    decorators

--- indexoffy/app/api/utils/decorators.py
+++ indexoffy/app/api/utils/decorators.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 
 from app import db
+from flask import request
 from functools import wraps
 
-class BaseApi(object):
+from app.models.base_token import BaseToken
+from app.models.base_customer import BaseCustomer
+from app.api.utils.responses import BaseResponse
+
+class BaseDecorator(object):
     """ Base View to Decorators common to all Webservices.
     """
 
@@ -11,3 +16,79 @@ class BaseApi(object):
         """Constructor
         """
         pass
+
+    def validate_token(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            access_token = request.headers.get('access_token')
+
+            response = BaseResponse(
+                model_class=str(__name__),
+                function="validate_token",
+            )
+
+            if not access_token:
+                return response.token_is_missing()
+            try:
+                query_user = db.session.query(
+                        BaseToken.id.label('token_id'),
+                        BaseCustomer.id.label('base_customer'),
+                        BaseCustomer.name.label('name'),
+                        BaseCustomer.email.label('email')
+                    ) \
+                    .join(BaseCustomer, BaseToken.base_customer==BaseCustomer.id) \
+                    .filter(
+                        BaseToken.api_token == access_token,
+                        BaseToken.status == True
+                    ).first()
+            except:
+                return response.token_is_invalid()
+
+            if query_user:
+                data =  {
+                    "request": request,
+                    "user": query_user,
+                }
+                return f(data, *args, **kwargs)
+        
+        return decorated
+
+    def validate_token_admin(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            access_token = request.headers.get('access_token')
+
+            response = BaseResponse(
+                token=access_token,
+                model_class=str(__name__),
+                function="validate_token_admin",
+                operation=str(['GET'])
+            )
+
+            if not access_token:
+                return response.token_is_missing()
+            try:
+                query_user = db.session.query(
+                        BaseToken.id.label('token_id'),
+                        BaseCustomer.id.label('base_customer'),
+                        BaseCustomer.name.label('name'),
+                        BaseCustomer.email.label('email')
+                    ) \
+                    .join(BaseCustomer, BaseToken.base_customer==BaseCustomer.id) \
+                    .filter(
+                        BaseToken.api_token == access_token,
+                        BaseToken.status == True
+                    ).first()
+            except:
+                return response.token_is_invalid()
+
+            if query_user:
+                data =  {
+                    "request": request,
+                    "user": query_user,
+                }
+                return f(data, *args, **kwargs)
+
+            return response.permission_denied() 
+        
+        return decorated
