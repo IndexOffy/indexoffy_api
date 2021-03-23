0de3909c9b7711011da0195e7f15edd7b5c8c124
FernandoCelmer <email@fernandocelmer.com>
2021-03-22 21:47:29 -0300

    app

--- indexoffy/app/__init__.py
+++ indexoffy/app/__init__.py
@@ -36,8 +36,8 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.modules.api_doc.controllers import mod_doc as doc
-from app.modules.api_user.controllers import mod_user as user
+from app.modules.public.api_doc.routers import mod_doc as doc
+from app.modules.public.api_user.routers import mod_user as user
 
 # Register blueprint(s)
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
--- indexoffy/app/modules/api_doc/controllers.py
+++
@@ -1,9 +0,0 @@
-# Import flask dependencies
-from flask import Blueprint, redirect, url_for
-
-mod_doc = Blueprint('', __name__, url_prefix='')
-
-# Set the route and accepted methods
-@mod_doc.route('/', methods=['GET'])
-def home():
-    return redirect("/docs/")
