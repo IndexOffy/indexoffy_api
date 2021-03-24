277b9a479eabeef0b9ac7a3005768c00fac81836
FernandoCelmer <email@fernandocelmer.com>
2021-03-23 23:38:34 -0300

    Formatação da rota referente a documentação da API

--- indexoffy/app/modules/public/api_doc/routes.py
+++ indexoffy/app/modules/public/api_doc/routes.py
@@ -4,6 +4,13 @@ from flask import Blueprint, redirect, url_for
 mod_doc = Blueprint('', __name__, url_prefix='')
 
 # Set the route and accepted methods
-@mod_doc.route('/', methods=['GET'])
-def home():
-    return redirect("/docs/")
+
+class ViewDoc(object):
+    """
+    """
+    def __init__(self):
+        super().__init__()
+
+    @mod_doc.route('/', methods=['GET'])
+    def home():
+        return redirect("/docs/")
