595c4bbbf1cdafcc709bd8ee54fe2fd40baec604
FernandoCelmer <email@fernandocelmer.com>
2021-03-25 03:34:09 -0300

    Implementação BaseHelper

---
+++ indexoffy/app/api/utils/helpers.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from app import db
+
+class BaseHelper(object):
+    """ Base View to Helper common to all Webservices.
+    """
+    def __init__(self, *args, **kwargs):
+        """Constructor
+        """
+
+    def helper(self):
+        """ 
+        """
+        return None
