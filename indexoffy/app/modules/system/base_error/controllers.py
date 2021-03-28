68006d6a3cacf928f1e24ccf7f5f7f442287dbe9
FernandoCelmer <fernando.celmer@cakeerp.com>
2021-03-28 01:46:55 -0300

    base_error

--- indexoffy/app/modules/system/base_error/controllers.py
+++ indexoffy/app/modules/system/base_error/controllers.py
@@ -3,18 +3,34 @@
 from app import db
 from app.api.base import BaseApi
 
+from app.models.base_error import BaseError, base_errors_schema
 
 class ControlerBaseError(BaseApi):
     """
     """
-    def __init__(self, data):
-        super().__init__(data)
+    def __init__(self, base_customer):
+        super().__init__(base_customer)
 
-    def get(self):
+        self.base_customer = base_customer
+
+    def get(self, model_id=None):
+        try:
+            query = BaseError.query \
+                .filter(
+                    BaseError.id == model_id,
+                    BaseError.bustomer == self.base_customer) \
+                .first()
+
+            if query:
+                result = base_errors_schema.dump(query)
+                return self.response.successfully_fetched()
+        except:
+            return self.response.server_error()
+
+        return self.response.user_dont_exist()
+
+    def post(self, data):
         pass
 
-    def post(self):
-        pass
-
-    def put(self):
+    def put(self, id, data):
         pass
