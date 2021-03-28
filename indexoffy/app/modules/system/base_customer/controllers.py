a41bd15d519a009fba4a3a79a853fe46aea4b7b1
Fernando Henrique de Oliveira Celmer <fernando.celmer@cakeerp.com>
2021-03-28 00:44:59 -0300

    app

--- indexoffy/app/modules/system/base_customer/controllers.py
+++ indexoffy/app/modules/system/base_customer/controllers.py
@@ -3,18 +3,33 @@
 from app import db
 from app.api.base import BaseApi
 
+from app.models.base_customer import BaseCustomer, base_customers_schema
 
 class ControlerBaseCustomer(BaseApi):
     """
     """
-    def __init__(self, data):
-        super().__init__(data)
+    def __init__(self, base_customer):
+        super().__init__(base_customer)
 
-    def get(self):
+        self.base_customer = base_customer
+
+    def get(self, id):
+        try:
+            query = BaseCustomer.query \
+                .filter(
+                    BaseCustomer.id == self.base_customer) \
+                .first()
+
+            if query:
+                result = base_customers_schema.dump(query)
+                return self.response.successfully_fetched()
+        except:
+            return self.response.server_error()
+
+        return self.response.user_dont_exist()
+
+    def post(self, data):
         pass
 
-    def post(self):
-        pass
-
-    def put(self):
+    def put(self, id, data):
         pass
