65af97d1446ce96316550ab3581c3b3850f3c3c9
FernandoCelmer <fernando.celmer@cakeerp.com>
2021-03-28 03:22:52 -0300

    Implementação função padrão get

--- indexoffy/app/api/base.py
+++ indexoffy/app/api/base.py
@@ -10,18 +10,38 @@ from app.api.utils.responses import BaseResponse
 class BaseApi(object):
     """ Base View to create helpers common to all Webservices.
     """
-    def __init__(self, data):
+    def __init__(self, base_customer, data=None):
         """Constructor
         """
-        pass
+        self.base_customer = base_customer
+        self.data = data
+        self.model_class = None
+        self.base_schema = None
+        self.base_schemas = None
+        self.response = BaseResponse(
+            data=data,
+            model_class=str(self.model_class)
+        )
 
-    def get(self):
+    def get(self, model_id=None):
+        try:
+            query = self.model_class.query \
+                .filter(
+                    self.model_class.id == model_id,
+                    self.model_class.base_customer == self.base_customer) \
+                .first()
+            if query:
+                result = self.base_schema.dump(query)
+                return self.response.successfully_fetched(result=result)
+        except:
+            return self.response.server_error()
+
+        return self.response.data_not_found()
+
+    def put(self, model_id=None):
         pass
 
     def post(self):
-        pass
-
-    def put(self):
         pass
 
     def delete(self):
