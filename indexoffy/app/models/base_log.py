a0345a69ddeb790316af9ec10fafa02f6755cf89
FernandoCelmer <email@fernandocelmer.com>
2021-04-03 01:35:39 -0300

    Alteração model base_log

--- indexoffy/app/models/base_log.py
+++ indexoffy/app/models/base_log.py
@@ -12,7 +12,7 @@ class BaseLog(Base):
     
     __tablename__ = 'base_log'
 
-    base_customer = db.Column(db.Integer, nullable=False)
+    base_customer = db.Column(db.Integer, nullable=True)
     user_name = db.Column(db.String(100), nullable=True)
     user_email = db.Column(db.String(100), nullable=True)
     operation = db.Column(db.String(10), nullable=True)
