eccf5a8b865cd3e47a462a2fcb9a6b99a6a18b8b
FernandoCelmer <email@fernandocelmer.com>
2021-04-03 01:33:44 -0300

    Implementação PUT e POST

--- indexoffy/app/api/base.py
+++ indexoffy/app/api/base.py
@@ -3,6 +3,7 @@
 from app import db
 from flask import jsonify, request
 from functools import wraps
+from traceback import format_exc
 
 from app.api.utils.responses import BaseResponse
 
@@ -18,11 +19,12 @@ class BaseApi(object):
         self.base_schema = None
         self.base_schemas = None
         self.base_customer = base_customer
-        self.response = BaseResponse(
-            base_customer=base_customer
-        )
+        self.response = BaseResponse(base_customer=base_customer)
+
 
     def get(self, model_id):
+        """ Method GET
+        """
         try:
             query = self.model_class.query \
                 .filter(
@@ -38,10 +40,43 @@ class BaseApi(object):
         return self.response.data_not_found()
 
     def put(self, model_id):
+        """ Method PUT
+        """
+        model_data = self.model_class.query.get(model_id)
+
+        # Verifica se existe registro.
+        if not model_data:
+            return self.response.user_dont_exist()
+
+        # Verificar conteúdo do registro.
+        if "id" in self.data['body']:
+            return self.response.invalid_data()
+
+        try:
+            for item in self.data['body']:
+                setattr(model_data, item, self.data['body'][item])
+            db.session.commit()
+            result = self.base_schema.dump(model_data)
+        except Exception as error:
+            db.session.rollback()
+
         return self.response.successfully_fetched()
 
     def post(self):
-        return self.response.successfully_fetched()
+        """ Method POST
+        """
+        try:
+            base_data = self.model_class(**self.data['body'])
+            db.session.add(base_data)
+            db.session.commit()
+            result = self.base_schema.dump(base_data)
+
+            return self.response.successfully_fetched(result=result)
+        except Exception as error:
+            db.session.rollback()
+            return self.response.server_error()
 
     def delete(self):
+        """ Method DELETE
+        """
         return self.response.successfully_fetched()
