a6840774f6e7f4f694352fe79ba05b73757ffcdc
FernandoCelmer <email@fernandocelmer.com>
2021-04-04 22:29:33 -0300

    Implementação decorators

--- indexoffy/app/api/utils/decorators.py
+++ indexoffy/app/api/utils/decorators.py
@@ -5,6 +5,7 @@ import ast
 from app import app, db
 from flask import request
 from functools import wraps
+from app.api.utils.utils import BaseUtils
 
 from app.models.base_token import BaseToken
 from app.models.base_customer import BaseCustomer
@@ -23,105 +24,46 @@ class BaseDecorator(object):
     def validate_token(f):
         @wraps(f)
         def decorated(*args, **kwargs):
-            access_token = request.headers.get('access_token')
+            data = BaseUtils.get_data(request=request)
+            response = BaseResponse(base_customer=data['base_customer'], data=data)
 
-            response = BaseResponse(
-                model_class=str(__name__),
-                function="validate_token",
-            )
-
-            if not access_token:
-                return response.token_is_missing()
             try:
-                query_user = db.session.query(
-                        BaseToken.id.label('token_id'),
-                        BaseToken.api_type.label('api_type'),
-                        BaseCustomer.id.label('base_customer'),
-                        BaseCustomer.name.label('name'),
-                        BaseCustomer.email.label('email')
-                    ) \
-                    .join(BaseCustomer, BaseToken.base_customer==BaseCustomer.id) \
+                query_token = db.session.query(BaseToken) \
                     .filter(
-                        BaseToken.api_token == access_token,
+                        BaseToken.api_token == data["access_token"],
                         BaseToken.status == True
                     ).first()
             except:
-                return response.token_is_invalid()
+                return response.permission_denied()
 
-            if query_user:
-                data =  {
-                    "request": request,
-                    "user": query_user,
-                }
-                return f(data, *args, **kwargs)
+            if query_token:
+                if query_token.base_customer != data['base_customer']:
+                   return response.permission_denied() 
+
+                return f(data['base_customer'], *args, **kwargs)
 
             return response.permission_denied()
-        
-        return decorated
-
-    def validate_token_admin(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            access_token = request.headers.get('access_token')
-
-            response = BaseResponse(
-                model_class=str(__name__),
-                function="validate_token_admin",
-            )
-
-            if not access_token:
-                return response.token_is_missing()
-            try:
-                query_user = db.session.query(
-                        BaseToken.id.label('token_id'),
-                        BaseToken.api_type.label('api_type'),
-                        BaseCustomer.id.label('base_customer'),
-                        BaseCustomer.name.label('name'),
-                        BaseCustomer.email.label('email')
-                    ) \
-                    .join(BaseCustomer, BaseToken.base_customer==BaseCustomer.id) \
-                    .filter(
-                        BaseToken.api_token == access_token,
-                        BaseToken.status == True,
-                        BaseToken.api_type == 0
-                    ).first()
-            except:
-                return response.token_is_invalid()
-
-            if query_user:
-                data =  {
-                    "request": request,
-                    "user": query_user,
-                }
-                return f(data, *args, **kwargs)
-
-            return response.permission_denied() 
         
         return decorated
 
     def validate_token_system(f):
         @wraps(f)
         def decorated(*args, **kwargs):
-            model_id = request.headers.get('base_customer', "0")
-            access_token = request.headers.get('access_token')
+            data = BaseUtils.get_data(request=request)
+            response = BaseResponse(base_customer=data['base_customer'], data=data)
 
-            if model_id.isnumeric() == False:
-                return BaseResponse().invalid_data()
-            else:
-                response = BaseResponse(base_customer=model_id)
-
-                try:
-                    query_user = db.session.query(BaseCustomer.id) \
-                        .filter(
-                            BaseCustomer.id == model_id,
-                        ).first()
-                except:
-                    return BaseResponse().server_error()
+            try:
+                query_user = db.session.query(BaseCustomer.id) \
+                    .filter(
+                        BaseCustomer.id == data['base_customer'],
+                    ).first()
+            except:
+                return response.server_error()
 
                 if not query_user:
                     return response.permission_denied()
 
-            if access_token == app.config['SECRET_KEY']:
+            if data['access_token'] == app.config['SECRET_KEY']:
                 return f(query_user.id, *args, **kwargs)
 
             return response.permission_denied() 
@@ -131,28 +73,19 @@ class BaseDecorator(object):
     def system(f):
         @wraps(f)
         def decorated(*args, **kwargs):
-            request_id = request.view_args.get('id', "0")
-            request_body = request.data.decode("UTF-8")
+            data = BaseUtils.get_data(request=request)
+            response = BaseResponse(base_customer=data['base_customer'], data=data)
 
-            if request_id != "0":
-                if request_id.isnumeric() == False:
-                    return BaseResponse().invalid_data()
+            if data["method"] in('PUT'):
+                if not data["id"]:
+                    return response.permission_denied()
 
-            if request_body:
-                request_body = ast.literal_eval(request_body)
-            else:
-                request_body = {}
+            if not data["base_customer"]:
+                return response.base_customer_is_missing()
 
-            data= {
-                "id": request_id,
-                "body": request_body,
-                "params": request.args,
-                "args": request.view_args,
-                "limit": request.args.get('limit', 50),
-                "page": request.args.get('page', 1),
-                "request": request,
-            }
-            
+            if not data["access_token"]:
+                return response.token_is_missing()
+
             return f(data, *args, **kwargs)
         
         return decorated
