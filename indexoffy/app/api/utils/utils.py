389fa396ac3875238f04a4f997ac9e06ec57d042
FernandoCelmer <email@fernandocelmer.com>
2021-04-04 22:28:33 -0300

    Criação função get_data

--- indexoffy/app/api/utils/utils.py
+++ indexoffy/app/api/utils/utils.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
-from app import db
+import ast
 
+from app import db
 from app.api.utils.responses import BaseResponse
 
 class BaseUtils(object):
@@ -11,17 +12,40 @@ class BaseUtils(object):
         """Constructor
         """
 
-    def check_number(self, value):
+    def get_data(request):
         """
         """
-        pass
+        request_body = request.data.decode("UTF-8")
+        request_id = request.view_args.get('id', None)
+        base_customer = request.headers.get('base_customer', None)
+        access_token = request.headers.get('access_token', None)
 
-    def check_character(self, value):
-        """
-        """
-        return None
+        if request_id:
+            if request_id.isnumeric() == False:
+                request_id = None
+            else:
+                request_id = int(request_id)
 
-    def check_request(self, request):
-        """
-        """
-        return None
+        if base_customer:
+            if base_customer.isnumeric() == False:
+                base_customer = None
+            else:
+                base_customer = int(base_customer)
+
+        if request_body:
+            request_body = ast.literal_eval(request_body)
+        else:
+            request_body = {}
+
+        return {
+            "id": request_id,
+            "body": request_body,
+            "params": request.args,
+            "args": request.view_args,
+            "access_token": access_token,
+            "base_customer": base_customer,
+            "limit": request.args.get('limit', 50),
+            "page": request.args.get('page', 1),
+            "method": request.method,
+            "request": request
+        }
